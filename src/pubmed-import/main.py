34ec9bca1e2c7aa069671e340f3bfd6c0598268e
Fei Phoon <fei.phoon@gmail.com>
2021-09-05 00:37:56 +0100

    Improve logging queries for run for pymed

--- src/pubmed-import/main.py
+++ src/pubmed-import/main.py
@@ -4,6 +4,7 @@ https://github.com/gijswobben/pymed/blob/master/examples/advanced_search/main.py
 Run this file from this path:
 python src/pubmed-import/main.py
 """
+import json
 from datetime import datetime
 from pathlib import Path
 from pymed import PubMed
@@ -14,11 +15,22 @@ output_path = Path(f"data/pymed-output/{output_run}")
 output_path.mkdir(parents=True, exist_ok=True)
 
 pubmed = PubMed(tool="MyTool", email="my@email.address")
-results = pubmed.query("ginseng", max_results=100)
+
+query = "ginseng"
+num_queries = 100
+query_results = pubmed.query(query, max_results=num_queries)
+
+_filepath = Path(f"{output_path}/query.txt")
+
+with _filepath.open("w", encoding="utf-8") as f:
+    query_info = {}
+    query_info["query"] = query
+    query_info["num_queries"] = num_queries
+    json.dump(query_info, f)
 
 _count = 0
 
-for r in results:
+for r in query_results:
     _filepath = Path(f"{output_path}/{str(_count)}.json")
 
     with _filepath.open("w", encoding="utf-8") as f:
