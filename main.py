2eabbfa6de75e65dec735a3d95d9ab011cdb1177
Fei Phoon <fei.phoon@gmail.com>
2021-10-20 13:14:11 +0100

    Comment out example statements

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from src.helpers.run_helper import Run
 if __name__ == "__main__":
     run = Run()
 
-    input_filepath = "banana"
-    print(run.create_run_filepath(input_filepath))
+    # Examples
+    # input_filepath = "banana"
+    # print(run.create_run_filepath(input_filepath))
 
-    output_filepath = "potato"
-    print(run.create_run_filepath(output_filepath))
+    # output_filepath = "potato"
+    # print(run.create_run_filepath(output_filepath))
+
