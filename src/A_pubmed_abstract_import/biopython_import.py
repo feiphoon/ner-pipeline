a10b801c7412c340e3de3bb7971779d222db6796
Fei Phoon <fei.phoon@gmail.com>
2021-10-21 00:46:50 +0100

    Refactor biopython import

--- src/A_pubmed_abstract_import/biopython_import.py
+++ src/A_pubmed_abstract_import/biopython_import.py
@@ -1,59 +1,209 @@
 """
-https://towardsdatascience.com/write-a-document-classifier-in-less-than-30-minutes-2d96a8a8820c
+Using the biopython.Bio library - example script here:
+https://github.com/biopython/biopython/blob/master/Scripts/query_pubmed.py
 
-Run this file from this path:
-python src/biopython-import/main.py
+More:
+https://towardsdatascience.com/write-a-document-classifier-in-less-than-30-minutes-2d96a8a8820c
 """
 import json
-from datetime import datetime
+
+from datetime import date
 from pathlib import Path
 from Bio import Entrez
+from typing import List, Any, Union
 
 
 Entrez.email = "my@email.address"
 Entrez.tool = "biopython"
 
-output_run = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
-output_path = Path(f"data/biopython-output/{output_run}")
-output_path.mkdir(parents=True, exist_ok=True)
+
+# The following declarations aren't used because I had trouble unpacking namedtuples
+# and dataclasses to JSON with the added complication of trying to serialise dates to string.
+
+# class PubmedAbstractObject(NamedTuple):
+#     pmid: str = None
+#     doi: str = None
+#     language: List[str] = None
+#     title: str = None
+#     abstract: str = None
+#     keywords: List[str] = None
+#     article_date: date = None
+#     date_completed: date = None
+#     date_revised: date = None
 
 
-def search(query: str, max_documents: int = 100) -> list:
-    handle = Entrez.esearch(
+# PubmedAbstractObject = NamedTuple(
+#     "PubmedAbstractObject",
+#     [
+#         ("pmid", str),
+#         ("doi", str),
+#         ("language", List[str]),
+#         ("title", str),
+#         ("abstract", str),
+#         ("keywords", List[str]),
+#         ("article_date", date),
+#         ("date_completed", date),
+#         ("date_revised", date),
+#     ],
+# )
+
+
+# PubmedAbstractObject = NamedTuple(
+#     "PubmedAbstractObject",
+#     pmid=str,
+#     doi=str,
+#     language=List[str],
+#     title=str,
+#     abstract=str,
+#     keywords=List[str],
+#     article_date=date,
+#     date_completed=date,
+#     date_revised=date,
+# )
+
+
+def import_pubmed_abstracts(
+    run_filepath: Path,
+    query_terms: List[str],
+    num_results: int,
+    pubmed_login: Entrez = Entrez,
+) -> None:
+    run_filepath.mkdir(parents=True, exist_ok=True)
+
+    query_str: str = "salvia hispanica L. chia"
+
+    query_results = search(pubmed_login, query_str, num_results)
+    query_id_list = query_results.get("IdList", [])
+    _query_metadata_filepath: str = Path(f"{run_filepath}/query.json")
+
+    with _query_metadata_filepath.open("w", encoding="utf-8") as f:
+        query_info: dict = {}
+        query_info["query_terms"] = query_str
+        query_info["num_results_requested"] = num_results
+        query_info["query_id_list"] = query_id_list
+        json.dump(query_info, f)
+
+    _success_count: int = 0
+    _reject_count: int = 0
+
+    run_reject_filepath: Path = Path(f"{run_filepath}/rejected/")
+    run_reject_filepath.mkdir(parents=True, exist_ok=True)
+
+    for query_id in query_id_list:
+        pubmed_abstract_dict = construct_pubmed_abstract_object(query_id)
+
+        has_abstract: bool = check_if_has_abstract(result=pubmed_abstract_dict)
+
+        if has_abstract:
+            _each_result_filepath: Path = Path(
+                f"{run_filepath}/{str(_success_count).zfill(3)}.json"
+            )
+            _success_count += 1
+        else:
+            _each_result_filepath: Path = Path(
+                f"{run_reject_filepath}/{str(_reject_count).zfill(3)}.json"
+            )
+            _reject_count += 1
+
+        with _each_result_filepath.open("w", encoding="utf-8") as f:
+            json.dump(pubmed_abstract_dict, f, default=convert_possible_date_to_str)
+
+
+def search(pubmed_login: Entrez, query: str, max_documents: int = 100) -> list:
+    handle = pubmed_login.esearch(
         db="pubmed", sort="relevance", retmax=max_documents, retmode="xml", term=query
     )
-    return Entrez.read(handle)
+    return pubmed_login.read(handle)
 
 
-def fetch_details(id: str) -> dict:
+def construct_pubmed_abstract_object(id: str) -> dict:
     handle = Entrez.efetch(
         db="pubmed",
         id=id,
-        rettype="xml",
-        retmode="text",
+        retmode="xml",
     )
-    records = Entrez.read(handle)
-    return records
-    # abstracts = [pubmed_article["MedlineCitation"]["Article"]["Abstract"]["AbstractText"][0] for pubmed_article in records["PubmedArticle"] if "Abstract" in pubmed_article["MedlineCitation"]["Article"].keys()]
-    # return abstracts
+    pubmed_article = Entrez.read(handle)
+
+    _pubmed_abstract_dict: dict = {}
+
+    _pubmed_abstract_dict["pmid"] = str(
+        pubmed_article["PubmedArticle"][0]["MedlineCitation"]["PMID"]
+    )
+    _pubmed_abstract_dict["doi"] = str(
+        pubmed_article["PubmedArticle"][0]["MedlineCitation"]["Article"]["ELocationID"][
+            0
+        ]
+    )
+    _pubmed_abstract_dict["language"] = str(
+        pubmed_article["PubmedArticle"][0]["MedlineCitation"]["Article"]["Language"]
+    )
+    _pubmed_abstract_dict["title"] = str(
+        pubmed_article["PubmedArticle"][0]["MedlineCitation"]["Article"]["ArticleTitle"]
+    )
+    _pubmed_abstract_dict["abstract"] = pubmed_article["PubmedArticle"][0][
+        "MedlineCitation"
+    ]["Article"]["Abstract"]["AbstractText"][0]
+    _pubmed_abstract_dict["keywords"] = list(
+        map(
+            str,
+            pubmed_article["PubmedArticle"][0]["MedlineCitation"]["KeywordList"][0],
+        )
+    )
+    _pubmed_abstract_dict["article_date"] = extract_article_date(
+        pubmed_article=pubmed_article, date_struct_name="ArticleDate"
+    )
+    _pubmed_abstract_dict["date_completed"] = extract_other_date(
+        pubmed_article=pubmed_article, date_struct_name="DateCompleted"
+    )
+    _pubmed_abstract_dict["date_revised"] = extract_other_date(
+        pubmed_article=pubmed_article, date_struct_name="DateRevised"
+    )
+
+    # pubmed_abstract_object = PubmedAbstractObject(**_pubmed_abstract_dict)
+    # return pubmed_abstract_object
+    return _pubmed_abstract_dict
 
 
-query = "ginseng"
-num_queries = 100
-query_results = search(query, num_queries)
-_filepath = Path(f"{output_path}/query.txt")
+def check_if_has_abstract(result: dict, tolerance: int = 100):
+    return len(result.get("abstract", "")) > tolerance
 
-with _filepath.open("w", encoding="utf-8") as f:
-    json.dump(query_results, f)
 
-doc_list = query_results.get("IdList", [])
+def convert_possible_date_to_str(obj: Any) -> Union[str, None]:
+    if isinstance(obj, date):
+        return obj.strftime("%Y%m%d")
+    else:
+        return obj
 
-_count = 0
-for _ in doc_list:
-    result = fetch_details(_)
 
-    _filepath = Path(f"{output_path}/{str(_count)}.json")
+def extract_article_date(pubmed_article: dict, date_struct_name: str) -> date:
+    _date_year = pubmed_article["PubmedArticle"][0]["MedlineCitation"]["Article"][
+        date_struct_name
+    ][0]["Year"]
+    _date_month = pubmed_article["PubmedArticle"][0]["MedlineCitation"]["Article"][
+        date_struct_name
+    ][0]["Month"]
+    _date_day = pubmed_article["PubmedArticle"][0]["MedlineCitation"]["Article"][
+        date_struct_name
+    ][0]["Day"]
+    return date(
+        year=int(_date_year),
+        month=int(_date_month),
+        day=int(_date_day),
+    )
 
-    with _filepath.open("w", encoding="utf-8") as f:
-        json.dump(result, f)
-    _count += 1
+
+def extract_other_date(pubmed_article: dict, date_struct_name: str) -> date:
+    _date_year = pubmed_article["PubmedArticle"][0]["MedlineCitation"][
+        date_struct_name
+    ]["Year"]
+    _date_month = pubmed_article["PubmedArticle"][0]["MedlineCitation"][
+        date_struct_name
+    ]["Month"]
+    _date_day = pubmed_article["PubmedArticle"][0]["MedlineCitation"][date_struct_name][
+        "Day"
+    ]
+    return date(
+        year=int(_date_year),
+        month=int(_date_month),
+        day=int(_date_day),
+    )
