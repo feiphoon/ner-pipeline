5c6d9cd97e99b1afb43de0da03d4c5ef41b1cd71
Fei Phoon <fei.phoon@gmail.com>
2021-10-20 18:12:55 +0100

    Refactor pymed_import method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.helpers.run_helper import Run
+from src.A_pubmed_abstract_import.pymed_import import import_pubmed_abstracts
 
 
 if __name__ == "__main__":
@@ -11,3 +12,9 @@ if __name__ == "__main__":
     # output_filepath = "potato"
     # print(run.create_run_filepath(output_filepath))
 
+    pubmed_abstracts_raw_filepath = "data/raw/pubmed_abstracts"
+    import_pubmed_abstracts(
+        run_filepath=run.create_run_filepath(pubmed_abstracts_raw_filepath),
+        query_terms=["ginseng", "salvia hispanica"],
+        num_results=5,
+    )
