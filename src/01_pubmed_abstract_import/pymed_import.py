d425e0057ddf1401e5782f1c4a60043c2b7a8cf7
Fei Phoon <fei.phoon@gmail.com>
2021-10-20 13:13:40 +0100

    Draft refactor of pubmed import function

--- src/01_pubmed_abstract_import/pymed_import.py
+++ src/01_pubmed_abstract_import/pymed_import.py
@@ -1,43 +1,46 @@
 """
+Using the pymed library - instructions here:
 https://github.com/gijswobben/pymed/blob/master/examples/advanced_search/main.py
-
-Run this file from this path:
-python src/pymed-import/main.py
 """
 import json
 from datetime import datetime
 from pathlib import Path
 from pymed import PubMed
 
+from src.helpers.run_helper import Run
 
-output_run = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
-output_path = Path(f"data/pymed-output/{output_run}")
-output_path.mkdir(parents=True, exist_ok=True)
 
-pubmed = PubMed(tool="pymed", email="my@email.address")
+PUBMED_LOGIN = PubMed(tool="pymed", email="my@email.address")
 
-query = "ginseng"
-num_queries = 100
-query_results = pubmed.query(query, max_results=num_queries)
 
-landed_query_results = [json.loads(_.toJSON()) for _ in query_results]
+def import_pubmed_abstracts(
+    run_filepath: Path,
+    query_terms: str,
+    num_results: int,
+    pubmed_login: PubMed = PUBMED_LOGIN,
+) -> None:
+    run_filepath.mkdir(parents=True, exist_ok=True)
+    pubmed = PUBMED_LOGIN
+    query_results = pubmed.query(query_terms, max_results=num_results)
 
-query_id_list = [_["pubmed_id"].split("\n")[0] for _ in landed_query_results]
-
-_filepath = Path(f"{output_path}/query.txt")
-
-with _filepath.open("w", encoding="utf-8") as f:
-    query_info = {}
-    query_info["query"] = query
-    query_info["num_queries"] = num_queries
-    query_info["query_id_list"] = query_id_list
-    json.dump(query_info, f)
-
-_count = 0
-
-for r in landed_query_results:
-    _filepath = Path(f"{output_path}/{str(_count)}.json")
+    landed_query_results = [json.loads(_.toJSON()) for _ in query_results]
+    query_id_list = [_["pubmed_id"].split("\n")[0] for _ in landed_query_results]
+    _filepath = Path(
+        f"{run_filepath}/query.json"
+    )  # TODO: might be a bug here - Path + str
 
     with _filepath.open("w", encoding="utf-8") as f:
-        json.dump(r, f)
-    _count += 1
+        query_info = {}
+        query_info["query"] = query
+        query_info["num_queries"] = num_queries
+        query_info["query_id_list"] = query_id_list
+        json.dump(query_info, f)
+
+    _count = 0
+
+    for r in landed_query_results:
+        _filepath = Path(f"{run_filepath}/{str(_count)}.json")
+
+        with _filepath.open("w", encoding="utf-8") as f:
+            json.dump(r, f)
+        _count += 1
