91b6c9696eb27c8955151338e2c78a46bd1e2a86
Fei Phoon <fei.phoon@gmail.com>
2021-10-21 03:50:14 +0100

    Delete example comments

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from src.helpers.run_helper import Run
 
-# from src.A_pubmed_abstract_import.pymed_import import import_pubmed_abstracts
 from src.A_pubmed_abstract_import.biopython_import import import_pubmed_abstracts
 
 
 if __name__ == "__main__":
     run = Run()
-
-    # Examples
-    # input_filepath = "banana"
-    # print(run.create_run_filepath(input_filepath))
-
-    # output_filepath = "potato"
-    # print(run.create_run_filepath(output_filepath))
 
     pubmed_abstracts_raw_filepath = "data/raw/pubmed_abstracts"
     terms = ["Salvia Hispanica L.", "chia"]
