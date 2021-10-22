55f74d28dc8ac850ff8cd28df2f0b48a1dca0ab5
Fei Phoon <fei.phoon@gmail.com>
2021-10-22 17:48:32 +0100

    Add processing to main pipeline

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.helpers.run_helper import Run
 
 from src.A_pubmed_abstract_import.biopython_import import import_pubmed_abstracts
+from src.B_pubmed_abstract_processing.biopython_processing import (
+    process_pubmed_abstracts,
+)
 
 
 if __name__ == "__main__":
@@ -14,3 +17,12 @@ if __name__ == "__main__":
             query_terms=t,
             num_results=3,
         )
+
+    pubmed_abstracts_processed_filepath = "data/processed/pubmed_abstracts"
+    process_pubmed_abstracts(
+        run_input_filepath=run.create_run_filepath(pubmed_abstracts_raw_filepath),
+        run_output_filepath=run.create_run_filepath(
+            pubmed_abstracts_processed_filepath
+        ),
+        abstract_size_tolerance=100,
+    )
