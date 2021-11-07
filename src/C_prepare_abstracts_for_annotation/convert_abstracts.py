5fe836f0149b4d4c86aa1d59122aa57c08b72b4a
Fei Phoon <fei.phoon@gmail.com>
2021-11-07 14:14:17 +0000

    Delete unneeded code

--- src/C_prepare_abstracts_for_annotation/convert_abstracts.py
+++ src/C_prepare_abstracts_for_annotation/convert_abstracts.py
@@ -25,32 +25,4 @@ def convert_abstracts(
     # Repartition to ballpark of 5 parquet files for real data
     df.coalesce(1).write.format("json").mode("overwrite").option(
         "schema", OUTPUT_SCHEMA
-
-
-# def prepare_abstracts_for_annotation(
-#     run_input_filepath: Path, run_output_filepath: Path
-# ) -> None:
-#     # Prepare the output filepath  - there will only be one
-#     run_output_filepath_complete: Path = Path(f"{run_output_filepath}/")
-#     run_output_filepath_complete.mkdir(parents=True, exist_ok=True)
-
-#     input_folder_paths: List = get_directories(run_input_filepath)
-
-#     # Per query folder, e.g. data/raw/pubmed_abstracts/2021-10-22-15-42-10/chia/
-#     for _input_folder in input_folder_paths:
-
-#         # Get all available file names in the input folder,
-#         # Exclude the metadata file.
-#         input_filepaths = [
-#             file for file in get_files(_input_folder) if "metadata" not in file.name
-#         ]
-
-#         # Open every file in this query folder
-#         for _input_file in input_filepaths:
-
-#             # Create a new
-#             with _input_file.open("r", encoding="utf-8") as f:
-#                 result = json.load(f)
-
-#                 result[]
     ).save(str(run_output_filepath))
