b3b942bfc89c12b899e3fed95174924f7c0ed869
Fei Phoon <fei.phoon@gmail.com>
2021-11-21 21:40:07 +0000

    Create schema for annotated abstracts

---
+++ src/F_synthesise_annotated_abstracts/schemas.py
@@ -0,0 +1,31 @@
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    StringType,
+    IntegerType,
+    ArrayType,
+)
+
+
+ANNOTATED_ABSTRACTS_INPUT_SCHEMA = StructType(
+    [
+        StructField("data", StringType(), True),
+        StructField("id", StringType(), True),
+        StructField("pmid", StringType(), True),
+        StructField("scientific_name_type", StringType(), True),
+        StructField(
+            "labels",
+            ArrayType(
+                StructType(
+                    [
+                        StructField("label_start_pos", IntegerType(), False),
+                        StructField("label_end_pos", IntegerType(), False),
+                        StructField("label_type", StringType(), False),
+                    ]
+                ),
+                False,
+            ),
+            False,
+        ),
+    ]
+)
