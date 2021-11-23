d849b923573b2385d1b588d73ba316ad9a336f84
Fei Phoon <fei.phoon@gmail.com>
2021-11-23 00:35:30 +0000

    Add schema to input abstracts, amend notes

--- src/F_synthesise_annotated_abstracts/synthesise_annotated_abstracts.py
+++ src/F_synthesise_annotated_abstracts/synthesise_annotated_abstracts.py
@@ -35,11 +35,11 @@ def synthesise_annotated_abstracts(
 
     name_mappings_df = name_mappings_df.transform(filter_out_empty_name_mappings)
 
-    # NOTE: This should not load train AND val - validation needs to be on an
-    # unseen pattern to see how well this generalises.
-    # It's the name mappings which need to change.
+    # NOTE: This is perfect that this loads only train. It's the name mappings
+    # which need to be split between train and val.
     annotated_abstracts_df: DataFrame = spark.read.json(
-        str(Path(f"{run_input_filepath}/{split_subset_type}/"))
+        str(Path(f"{run_input_filepath}/train/")),
+        schema=ANNOTATED_ABSTRACTS_INPUT_SCHEMA,
     )
 
     annotated_abstracts_df = annotated_abstracts_df.withColumn(
