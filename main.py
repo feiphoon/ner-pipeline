160a70d6d0e167b6c93f6eb85b73bb6cf64f490d
Fei Phoon <fei.phoon@gmail.com>
2021-11-30 13:27:44 +0000

    Get another batch of abstracts

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ if __name__ == "__main__":
     pubmed_abstracts_raw_filepath: str = "data/raw/pubmed_abstracts"
     # _daisy_terms = ["Bellis perennis L.", "daisy"]
     # _chia_terms = ["Salvia hispanica L.", "chia"]
-    _thyme_terms = ["Thymus quinquecostatus Celak.", "thyme", "thymi herba", "Thymus quinquecostatus Celakovski"]
+    # _thyme_terms = ["Thymus quinquecostatus Celak.", "thyme", "thymi herba", "Thymus quinquecostatus Celakovski"]
     _st_johns_wort_terms = ["Hypericum perforatum L.", "st. john's wort", "herba hyperici"]
     _pennywort_terms = ["Centella asiatica (L.) Urb.", "asiatic pennywort", "indian pennywort", "centellae asiaticae herba", "centellae folium", "centellae herba"]
-    terms = _pennywort_terms + _st_johns_wort_terms + _thyme_terms
+    terms = _pennywort_terms + _st_johns_wort_terms
     for t in terms:
         import_pubmed_abstracts(
             run_filepath=run.create_run_filepath(pubmed_abstracts_raw_filepath),
             query_terms=t,
-            num_results=30,
+            num_results=15,
         )
 
     pubmed_abstracts_processed_filepath: str = "data/processed/pubmed_abstracts"
