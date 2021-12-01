da4b531e267e4f373a9ad48dbd64fe5c0c0d8f96
Fei Phoon <fei.phoon@gmail.com>
2021-12-01 19:08:12 +0000

    Draft putting the run together

--- synthesise_annotated_abstracts_runner.py
+++ synthesise_annotated_abstracts_runner.py
@@ -2,13 +2,17 @@ from pathlib import Path
 from pyspark.sql import SparkSession
 
 from src.helpers.run import Run, get_last_run_timestamp
+from src.helpers.train_test_split import TrainTestSplit
+
 from src.F_synthesise_annotated_abstracts.prepare_annotated_abstracts_for_entity_replacement import (
     prepare_annotated_abstracts,
 )
-from src.helpers.train_test_split import TrainTestSplit
+
+from src.F_synthesise_annotated_abstracts.magic import perform_entity_replacement
 
 
 TRAIN_TEST_SPLIT = TrainTestSplit(0.5, 0.5)
+SEED = 1
 
 spark = SparkSession.builder.appName("run_ner_pipeline").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
@@ -28,7 +32,7 @@ prepare_annotated_abstracts(
     train_test_split=TRAIN_TEST_SPLIT,
     split_subset_type="train",
     exclude_scientific_name_ids=["wcs516286", "wcsCmp708815"],
-    seed=1,
+    seed=SEED,
 )
 
 # Real data run
@@ -47,5 +51,50 @@ prepare_annotated_abstracts(
 #     train_test_split=TRAIN_TEST_SPLIT,
 #     split_subset_type="train",
 #     exclude_scientific_name_ids=["wcs516286", "wcsCmp708815"],
-#     seed=1,
+#     seed=SEED,
 # )
+
+
+prepared_annotated_abstracts_filepath: str = (
+    "data/processed/synthesised_annotated_abstracts/A_prepared"
+)
+interim_scientific_names_replaced_abstracts_filepath: str = (
+    "data/processed/synthesised_annotated_abstracts/B_scientific_replaced"
+)
+perform_entity_replacement(
+    run_input_filepath=run.create_run_filepath(prepared_annotated_abstracts_filepath),
+    run_output_filepath=run.create_run_filepath(
+        interim_scientific_names_replaced_abstracts_filepath
+    ),
+    entity_type_to_replace="scientific",
+    seed=SEED,
+    sample_run=True,
+)
+
+interim_common_names_replaced_abstracts_filepath: str = (
+    "data/processed/synthesised_annotated_abstracts/C_common_replaced"
+)
+perform_entity_replacement(
+    run_input_filepath=run.create_run_filepath(
+        interim_scientific_names_replaced_abstracts_filepath
+    ),
+    run_output_filepath=run.create_run_filepath(
+        interim_common_names_replaced_abstracts_filepath
+    ),
+    entity_type_to_replace="common",
+    seed=SEED,
+)
+
+interim_pharmaceutical_names_replaced_abstracts_filepath: str = (
+    "data/processed/synthesised_annotated_abstracts/D_pharmaceutical_replaced"
+)
+perform_entity_replacement(
+    input_filepath=run.create_run_filepath(
+        interim_common_names_replaced_abstracts_filepath
+    ),
+    output_filepath=run.create_run_filepath(
+        interim_pharmaceutical_names_replaced_abstracts_filepath
+    ),
+    entity_type_to_replace="pharmaceutical",
+    seed=SEED,
+)
