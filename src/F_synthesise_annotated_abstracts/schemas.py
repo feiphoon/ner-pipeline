e871db09bc40b9b489e8635437ea21bde453808d
Fei Phoon <fei.phoon@gmail.com>
2021-12-01 03:02:08 +0000

    Add MPNS v5 schema

--- src/F_synthesise_annotated_abstracts/schemas.py
+++ src/F_synthesise_annotated_abstracts/schemas.py
@@ -3,6 +3,7 @@ from pyspark.sql.types import (
     StructField,
     StringType,
     ArrayType,
+    IntegerType,
 )
 
 
@@ -19,5 +20,47 @@ ANNOTATED_ABSTRACTS_INPUT_SCHEMA = StructType(
             ),
             False,
         ),
+    ]
+)
+
+
+MPNS_NAME_MAPPINGS_V5_SCHEMA = StructType(
+    [
+        StructField("mapping_id", IntegerType(), False),
+        StructField("scientific_name_id", StringType(), False),
+        StructField("scientific_name", StringType(), False),
+        StructField("scientific_name_type", StringType(), False),
+        StructField("scientific_name_length", IntegerType(), False),
+        StructField(
+            "common_names",
+            ArrayType(
+                StructType(
+                    [
+                        StructField("non_scientific_name", StringType(), False),
+                        StructField("non_scientific_name_length", IntegerType(), False),
+                        StructField("non_scientific_name_id", StringType(), False),
+                    ]
+                ),
+                True,
+            ),
+            True,
+        ),
+        StructField(
+            "pharmaceutical_names",
+            ArrayType(
+                StructType(
+                    [
+                        StructField("non_scientific_name", StringType(), False),
+                        StructField("non_scientific_name_length", IntegerType(), False),
+                        StructField("non_scientific_name_id", StringType(), False),
+                    ]
+                ),
+                True,
+            ),
+            True,
+        ),
+        StructField("non_scientific_name_count", IntegerType(), False),
+        StructField("common_name_count", IntegerType(), False),
+        StructField("pharmaceutical_name_count", IntegerType(), False),
     ]
 )
