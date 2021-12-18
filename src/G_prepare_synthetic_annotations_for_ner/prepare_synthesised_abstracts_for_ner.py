54fbab47401012067106b7a5028b552ad7ac8e56
Fei Phoon <fei.phoon@gmail.com>
2021-12-18 23:12:57 +0000

    Update with shuffle code

--- src/G_prepare_synthetic_annotations_for_ner/prepare_synthesised_abstracts_for_ner.py
+++ src/G_prepare_synthetic_annotations_for_ner/prepare_synthesised_abstracts_for_ner.py
@@ -9,11 +9,19 @@ from src.G_prepare_synthetic_annotations_for_ner.schemas import (
     SYNTHESISED_ANNOTATIONS_INPUT_SCHEMA,
 )
 
+#  Monkeypatch in case I don't use Spark 3.0
+def transform(self, f):
+    return f(self)
+
+
+DataFrame.transform = transform
+
 
 def prepare_synthesised_abstracts_for_ner(
     spark: SparkSession,
     run_input_filepath: Path,
     run_output_filepath: Path,
+    seed: int = 42,
 ) -> None:
 
     synthesised_abstracts_df: DataFrame = spark.read.json(
@@ -36,13 +44,25 @@ def prepare_synthesised_abstracts_for_ner(
     # Aggregrate the new entities columns to replace the old labels column
     synthesised_abstracts_df: DataFrame = synthesised_abstracts_df.withColumnRenamed(
         "label", "entities"
-    )
+    ).transform(lambda df: perform_deterministic_shuffle(df, seed))
 
     # Then write cleaned_data
     run_output_filepath.mkdir(parents=True, exist_ok=True)
 
     synthesised_abstracts_df.coalesce(1).write.format("json").mode("overwrite").save(
         str(run_output_filepath)
+    )
+
+
+def perform_deterministic_shuffle(df: DataFrame, seed: int) -> DataFrame:
+    """
+    Hacky method of doing a shuffle with deterministic results.
+    """
+    return (
+        df.orderBy(f.col("pmid").asc())
+        .withColumn("order", f.rand(seed=seed))
+        .orderBy(f.col("order").asc())
+        .drop("order")
     )
 
 
